11440e4a37dfb3a2301b53178c1cf9391355d360
Victor Garitskyu <victor_95_22@mail.ru>
2022-02-17 18:10:20 +0600

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Victor Garitskyu on 17.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
